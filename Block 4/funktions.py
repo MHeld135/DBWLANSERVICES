a3b1aad5a2b3ff1633b5a698f93f664d26a82d8a
Manfred Held <manfred.held@deutschebahn.com>
2024-11-25 09:58:42 +0100

    Ändern Funktion mit Message

---
+++ Block 4/funktions.py
@@ -0,0 +1,15 @@
+def function1() :
+    for_fn1 = 'Hello'
+    message = 'Global change by fn1'
+    print (f'Called function1, {for_fn1}, {message}')
+
+def function2() :
+    message_2 = 'Tschau'
+    print (f'Call function1, {message_1}, {message}')
+    print ('called function2 ')
+
+message = 'Global'
+
+function1()
+function2()
+
